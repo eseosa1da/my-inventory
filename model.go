8d0ac220899222e60cfc5b4daa29db3d4a5553e3
Eseosa Otasowie <eseosaotasowie@Eseosas-Air.ht.home>
2023-04-02 23:25:01 -0400

    Integrated with Github workflows pt6

--- model.go
+++ model.go
@@ -7,7 +7,7 @@ import (
 )
 
 type product struct {
-	ID       int     `json: "id"`
+	ID       int     `json:"id"`
 	Name     string  `json:"name"`
 	Quantity int     `json:"quantity"`
 	Price    float64 `json:"price"`
