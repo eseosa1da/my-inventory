5fdd79c077d91c3c4fcdbb0a03629326df042f78
Eseosa Otasowie <eseosaotasowie@Eseosas-MacBook-Air.local>
2023-04-05 12:45:38 -0400

    Integrated with Github workflows pt23

--- constants.go
+++ constants.go
@@ -10,7 +10,7 @@ func getEnvOrDefault(env, defaultVal string) string {
 	return defaultVal
 }
 
-var DBName = getEnvOrDefault("DB_NAME", "inventory")
+var DBName = getEnvOrDefault("DB_NAME", "inventorys")
 var DbUser = getEnvOrDefault("DB_USER", "admin")
 var DbPassword = getEnvOrDefault("DB_PASSWORD", "Password12")
 var DBHost = getEnvOrDefault("DB_HOST", "goproject.cwqtgpeadrce.us-east-1.rds.amazonaws.com:3306")
