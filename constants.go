9fc924211e9a433e6eeac70554967e94b55be0d4
Eseosa Otasowie <eseosaotasowie@Eseosas-MacBook-Air.local>
2023-04-05 12:59:01 -0400

    Revert "Integrated with Github workflows pt25"
    This reverts commit da5be831d9c96809733d0249e0dcd71ed02d7a34.

--- constants.go
+++ constants.go
@@ -1,14 +1,16 @@
 package main
 
-// func getEnvOrDefault(env, defaultVal string) string {
-// 	if val := os.Getenv(env); val != "" {
-// 		return val
-// 	}
+import "os"
 
-// 	return defaultVal
-// }
+func getEnvOrDefault(env, defaultVal string) string {
+	if val := os.Getenv(env); val != "" {
+		return val
+	}
 
-var DBName = "test"
-var DbUser = "root"
-var DbPassword = "Password12"
-var DBHost = "localhost"
+	return defaultVal
+}
+
+var DBName = getEnvOrDefault("DB_NAME", "test")
+var DbUser = getEnvOrDefault("DB_USER", "root")
+var DbPassword = getEnvOrDefault("DB_PASSWORD", "Password12")
+var DBHost = getEnvOrDefault("DB_HOST", "localhost")
