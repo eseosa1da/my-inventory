62359f03f5d902bc6dcc2dda442652c7645c3f64
Eseosa Otasowie <eseosaotasowie@Eseosas-MacBook-Air.local>
2023-04-05 12:43:12 -0400

    Revert "Integrated with Github workflows pt22"
    This reverts commit 6af76c8391ec1700b8d78eddd9c064f9c1769b45.

--- constants.go
+++ constants.go
@@ -2,22 +2,15 @@ package main
 
 import "os"
 
-// func getEnvOrDefault(env, defaultVal string) string {
-// 	if val := os.Getenv(env); val != "" {
-// 		return val
-// 	}
+func getEnvOrDefault(env, defaultVal string) string {
+	if val := os.Getenv(env); val != "" {
+		return val
+	}
 
-// 	return defaultVal
-// }
+	return defaultVal
+}
 
-// var DBName, DBUser, DBPassword, DBHost string
-
-var DBName = os.Getenv("DB_NAME")
-var DBUser = os.Getenv("DB_USER")
-var DBPassword = os.Getenv("DB_PASSWORD")
-var DBHost = os.Getenv("DB_HOST")
-
-// DBName = getEnvOrDefault("DB_NAME", "inventory")
-// DBUser = getEnvOrDefault("DB_USER", "admin")
-// DBPassword = getEnvOrDefault("DB_PASSWORD", "Password12")
-// DBHost = getEnvOrDefault("DB_HOST", "goproject.cwqtgpeadrce.us-east-1.rds.amazonaws.com:3306")
+var DBName = getEnvOrDefault("DB_NAME", "inventory")
+var DbUser = getEnvOrDefault("DB_USER", "admin")
+var DbPassword = getEnvOrDefault("DB_PASSWORD", "Password12")
+var DBHost = getEnvOrDefault("DB_HOST", "goproject.cwqtgpeadrce.us-east-1.rds.amazonaws.com:3306")
--- main.go
+++ main.go
@@ -3,6 +3,6 @@ package main
 func main() {
 	app := App{}
 
-	app.Initialize(DBUser, DBPassword, DBHost, DBName)
+	app.Initialize(DbUser, DbPassword, DBHost, DBName)
 	app.Run("localhost:10000")
 }
